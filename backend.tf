4886b7f0a2abbc102791e0bf3d90127214d7a1b4
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-06-09 19:53:41 +0000

    added lint, checkov

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "tf-bucket-prod1"
-    key          = "uc2-ha-latest/terraform.tfstate"
+    key          = "uc3-openproject/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
     encrypt      = true
